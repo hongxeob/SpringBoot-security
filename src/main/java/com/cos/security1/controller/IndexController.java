f4b5d6c51e3a80d7374527445fd4c67472d6a04a
hongxeob <sjun2918@naver.com>
2022-10-06 22:42:30 +0900

    시큐리티 회원가입(html 만들기/비밀번호 암호화하기)주석 메모 완료

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -16,54 +16,52 @@ public class IndexController {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
-	
-	@GetMapping({"","/"})
+
+	@GetMapping({ "", "/" })
 	public String index() {
-		//머스태치 기본 폴더 -> src/main/resource/
-		//뷰 리졸버 설정 : templates(prefix) .mustache(suffix) ->생략가능!
+		// 머스태치 기본 폴더 -> src/main/resource/
+		// 뷰 리졸버 설정 : templates(prefix) .mustache(suffix) ->생략가능!
 		return "index";
 	}
-	
+
 	@GetMapping("/user")
 	public @ResponseBody String user() {
 		return "user";
 	}
-	
+
 	@GetMapping("/admin")
 	public @ResponseBody String admin() {
 		return "admin";
 	}
-	
+
 	@GetMapping("/manager")
 	public @ResponseBody String manager() {
 		return "manager";
 	}
-	
-	//스프링시큐리티가 해당 주소를 낚아챔 -SecurityConfig 파일 생성 후 작동 안함.
+
+	// 스프링시큐리티가 해당 주소를 낚아챔 -SecurityConfig 파일 생성 후 작동 안함.
 	@GetMapping("/loginForm")
 	public String loginForm() {
 		return "loginForm";
 	}
-	
-	@PostMapping("/join") 
+
+	@PostMapping("/join")
 	public String join(User user) {
 		System.out.println(user);
 		user.setRole("ROLE_USER");
-		String rawPassword = user.getPassword();
-		String encPaswword = bCryptPasswordEncoder.encode(rawPassword);
-		user.setPassword(encPaswword);
-		userRepository.save(user); //회원가입은 잘됨.but 비밀번호 :1234 => 시큐리티 로그인을 할 수가 없음. 이유는 비밀번호가 암호화 안 되었기 떄문.
+		String rawPassword = user.getPassword(); //생 비밀번호(rawPass--)는 유저가 적은 걸 받은 패스워드이다.
+		String encPaswword = bCryptPasswordEncoder.encode(rawPassword); //암호화 패스워드는(encPass--) rawPass--를 암호화 한것이다 
+		user.setPassword(encPaswword); // DB로 날라오는것은 암호화된 encPass--이다.
+		userRepository.save(user); // 그런 후 그걸 유저 레파지토리에 저장해준다.
 		return "redirect:/loginForm";
 	}
-	
-	
+
 	@GetMapping("/joinForm")
 	public String joinForm() {
 		return "joinForm";
 	}
-	
-	
+
 }
