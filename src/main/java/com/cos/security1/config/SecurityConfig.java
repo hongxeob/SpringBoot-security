5c837cdfa0bd77912f0dafb94a0cf73aca75cbea
hongxeob <sjun2918@naver.com>
2022-10-09 18:14:38 +0900

    구글 회원 프로필 정보 받기

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.cos.security1.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -9,11 +10,16 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.cos.security1.config.oauth.PrincipalOauth2UserService;
+
 @EnableWebSecurity //스프링 시큐리티 필터가 스프링 필터체인에 등록이 됨.
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true) //secure 어노테이션 활성화 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	
+	@Autowired
+	private PrincipalOauth2UserService principalOauth2UserService;
 	//해당 메소드의 리턴되는 오브젝트를 IoC로 등록해준다.
 	@Bean
 	public BCryptPasswordEncoder encodePwd() {
@@ -36,7 +42,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.defaultSuccessUrl("/")
 			.and()
 			.oauth2Login()
-			.loginPage("/loginForm"); //구글 로그인이 완료된 뒤의 후처리가 필요.
+			.loginPage("/loginForm")
+			.userInfoEndpoint()
+			.userService(principalOauth2UserService); //구글 로그인이 완료된 뒤의 후처리가 필요.
 			
 		
 	}
